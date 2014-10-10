19f721ecefc2051e1fbb361505fb064e56adfb97
a1657884 <a1657884@21812873-c04a-464d-95cf-c10f675487a6>
2014-10-10 08:46:25 +0000

    8

--- 2014/s2/adds/assignment8/main.cpp
+++ 2014/s2/adds/assignment8/main.cpp
@@ -9,7 +9,7 @@ class Sort{										//sort class
 public:
     int data[];									//array, each element is number of input
     int temp;									//template number, swap numbers when sorting
-    int length;                                 //the length od the data[] array
+    int length;                                 //the length of the data[] array
 };
 
 class BubbleSort: public Sort{                  //Bubblesort class, child class of the sort
@@ -37,69 +37,68 @@ public:
         int first=low;
         int last=high;
         int key=a[first];
-        while(first<last){
-            while(first<last&&a[last]>=key)
-            --last;
+        while(first<last){						//if the right number is smaller than left number
+            while(first<last&&a[last]>=key)			//then swap 2 numbers and last should -1 and 
+            --last;									//first should +1
             a[first]=a[last];
             while(first<last&&a[first]<=key)
             ++first;
             a[last]=a[first];
         }
         a[first]=key;
-        Qsort(a,low,first-1);
-        Qsort(a,first+1,high);
+        Qsort(a,low,first-1);					//use recursion to continue this function and in the 
+        Qsort(a,first+1,high);						//end. the function will return the sorted array
 }
 };
 
 class RecursiveBinarySearch{                        // binary search class
 public:
     int Search(int b[],int left,int right,int key){ // binary search function
-        if(left>right){                             // the first principle simila with
+        if(left>right){                             // the first principle similar with
             cout<<"false"<<" ";                         //quick sort because they all
             return 0;                                   //use recursion. first we need to
         }                                               //set the output of the function
         else{
-            int middle=(left+right)/2;
-            if (b[middle]==0){
-                cout<<"true"<<" ";
+            int middle=(left+right)/2;				//set the middle number 
+            if (b[middle]==key){						//if the middle number is 0, then return and output 
+                cout<<"true"<<" ";						//true
                 return 0;
             }
-            else if(0<b[middle]){
-            right=middle-1;
-            return Search(b,left,right,key);
+            else if(key<b[middle]){					// if in the sorted array, the middle number is less than 
+            right=middle-1;								//0, then use recursion, if can not find the number,
+            return Search(b,left,right,key);			//return false, else return true
             }
-            else{
-            left=middle+1;
+            else{									// same as recursion above, the only difference is in this case
+            left=middle+1;								//middle number is less than key
             return Search(b,left,right,key);
             }
         }
     }
 };
 
-int main()
-{
-    string input;
-    int space=1;
-    getline(cin,input);
+int main(){											//main function
+    string input;									// input is the array you should input
+    int space=1;									//set space, the space+1 will be the length of array
+    getline(cin,input);								// use getline and istringstream to set divide each element by ' '
     istringstream str1(input);
-    for(int i=0;i<input.length();i++){
+    for(int i=0;i<input.length();i++){				// for loop to decide space
         if (input[i]==' '){
             space++;
         }
     }
-    int numbers[space];
-    string array_string[space];
-    for(int j=0;j<space;j++){
-        str1>>array_string[j];
-        numbers[j]=(atoi(array_string[j].c_str()));
+    int numbers[space];								//set number[] which the element is int
+    string array_string[space];						//set array_string[] which the element is string
+    for(int j=0;j<space;j++){						//use for loop and istreamstring to transfer each element 
+        str1>>array_string[j];							//to array_string[] then use atoi change array type from
+        numbers[j]=(atoi(array_string[j].c_str()));		//string to int
     }
-    QuickSort case1;
-    case1.Qsort(numbers,0,sizeof(numbers)/sizeof(numbers[0])-1);
+    QuickSort case1;													//object case1
+    case1.Qsort(numbers,0,sizeof(numbers)/sizeof(numbers[0])-1);		//quicksort numbers array
 
-    RecursiveBinarySearch case2;
-    case2.Search(numbers,0,sizeof(numbers)/sizeof(numbers[0])-1,0);
+    RecursiveBinarySearch case2;										//object case2
+    case2.Search(numbers,0,sizeof(numbers)/sizeof(numbers[0])-1,0);		//seach hte sorted array and print out true or false
 
-    for(int i=0;i<sizeof(numbers)/sizeof(numbers[0]);i++){
+    for(int i=0;i<sizeof(numbers)/sizeof(numbers[0]);i++){				//print out the sorted array
     cout<<numbers[i]<<" ";
     }
     return 0;
