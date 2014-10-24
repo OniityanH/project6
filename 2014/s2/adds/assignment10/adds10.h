5877273e8afd5a95841b15afb1629c0638f925c4
a1657884 <a1657884@21812873-c04a-464d-95cf-c10f675487a6>
2014-10-24 07:12:30 +0000

    dsdas

---
+++ 2014/s2/adds/assignment10/adds10.h
@@ -0,0 +1,17 @@
+#ifndef ADDS10_H
+#define ADDS10_H
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+#include <stdlib.h>
+#include <stack>
+using namespace std;
+
+class Polish{                                                               //set a class which include a function
+public:
+    void calculation(int a[],string b[],int size_of_a, int size_of_b);      //set the function which include 4 input numbers,number array a[],symbol array b[],size of a[], size of b[]
+};
+
+#endif // ADDS10_H
